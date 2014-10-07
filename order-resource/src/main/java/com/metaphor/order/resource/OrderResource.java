5fa93a40c92791ab8a92d5bbfad9c0ef038a4a81
yan qian <qianyan@thoughtworks.com>
2014-10-07 19:32:48 +0800

    [Yan Qian] change the description of order resource api.

--- order-resource/src/main/java/com/metaphor/order/resource/OrderResource.java
+++ order-resource/src/main/java/com/metaphor/order/resource/OrderResource.java
@@ -3,9 +3,7 @@ package com.metaphor.order.resource;
 import com.google.common.collect.ImmutableList;
 import com.metaphor.order.client.model.Order;
 import com.metaphor.order.service.OrderService;
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.*;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -17,7 +15,7 @@ import java.util.List;
 
 @Path("order")
 @Produces(MediaType.APPLICATION_JSON)
-@Api(value="order", description = "order resource")
+@Api(value="/order", description = "order resource")
 public class OrderResource {
 
     private final OrderService service;
@@ -29,7 +27,9 @@ public class OrderResource {
 
     @GET
     @Path("/{numbers}")
-    @ApiOperation("get orders via some orders' number")
+    @ApiOperation(value = "get orders via some orders' number", responseContainer = "List")
+    @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid numbers supplied"),
+            @ApiResponse(code = 404, message = "Order not found") })
     public List<Order> getOrders(@PathParam("numbers")
                                  @ApiParam(value = "some orders' number", allowMultiple = true) String numbers) {
         return service.orders(numbers);
@@ -37,12 +37,15 @@ public class OrderResource {
 
     @GET
     @Path("/cancelableOrder/{numbers}")
-    @ApiOperation("Practice: get some orders that can be cancelled, " +
-            "that means you can cancel order only when " +
+    @ApiOperation(value="get some orders can be cancelled,",
+            notes = "that means you can cancel order only when " +
             "the order's status is COMMIT or the status is ACCEPTED " +
-            "but the day after order time is still in two days")
-    public List<Order> canCancelledOrders(@PathParam("numbers")
-                                          @ApiParam(value = "some can be cancelled orders' number", allowMultiple = true) String numbers) {
+            "but the day after order time is still in two days",
+            responseContainer = "List")
+    @ApiResponses(value = { @ApiResponse(code = 400, message = "Invalid numbers supplied"),
+            @ApiResponse(code = 404, message = "Order not found") })
+    public List<Order> canCancelledOrders(@ApiParam(value = "some can be cancelled orders' number", allowMultiple = true)
+                                          @PathParam("numbers") String numbers) {
 
         return ImmutableList.of();
     }
